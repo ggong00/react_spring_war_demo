28e84b565e3d47882e28d913c8fccb6832b92b86
이현주 <hyeonju3457@gmail.com>
2022-12-12 17:33:02 +0900

    수정 - 로그인 - ui 수정

--- src/main/frontend/src/components/login/Login.js
+++ src/main/frontend/src/components/login/Login.js
@@ -37,41 +37,66 @@ function Login(props) {
     }
 
     return (
-        <div id="route-contents">
+        // <div id="route-contents">
             <div className="login-contents">
-                <form className="section1" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="logo">
-                        <img src={require("../../assets/img/common/logo.png")} />
+                <div className="login_wrap">
+                    <form className="" onSubmit={handleSubmit(onSubmit)} >
+                        <div className="logo">
+                            <img src={require("../../assets/img/common/logo.png")} />
+                        </div>
+                        <div className="login-input">
+
+
+                            <div className="input_group">
+                                <label htmlFor="idInput">아이디</label>
+                                <input
+                                    className="form_control"
+                                    id="idInput"
+                                    type="text"
+                                    name="userId"
+                                    {...register("userId")}
+                                />
+                            </div>
+                            <div className="input_group">
+                                <label htmlFor="PassInput">비밀번호</label>
+                                <input
+                                    className="form_control"
+                                    id="PassInput"
+                                    type="password"
+                                    name="userPass"
+                                    {...register("userPass")}
+                                />
+                            </div>
+                            <div className="btn-submit">
+                                <button className="btn" type="submit">로그인</button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <div className="snsLoginArea">
+                        <p><span>SNS 로그인</span></p>
+                        <ul>
+                            <li><button className="btn" style={{background:'#00c73c', border: '1px solid #00c73c', color: '#fff'}}>
+                                <img className="ico" src={require("../../assets/img/login/ico_naver.png")}/>
+                                네이버로 로그인
+                            </button></li>
+                            <li><button className="btn" style={{background: '#fff', border: '1px solid #dadce0'}}>
+                                <img className="ico" src={require("../../assets/img/login/ico_google.png")}/>
+                                구글로 로그인
+                            </button></li>
+                            <li><button className="btn" style={{background:'#fef124', border: '1px solid #fef124', color: '#3e2723'}}>
+                                <img className="ico" src={require("../../assets/img/login/ico_kakao.png")}/>
+                                카카오로 로그인
+                            </button></li>
+                        </ul>
+
                     </div>
-                    <div className="login-input">
-                        <div className="input">
-                            <label htmlFor="idInput">아이디</label>
-                            <input
-                                id="idInput"
-                                type="text"
-                                name="userId"
-                                {...register("userId")}
-                            />
-                        </div>
-                        <div className="input">
-                            <label htmlFor="PassInput">비밀번호</label>
-                            <input
-                                id="PassInput"
-                                type="password"
-                                name="userPass"
-                                {...register("userPass")}
-                            />
-                        </div>
-                        <div className="btn-submit">
-                            <button type="submit">로그인</button>
-                        </div>
-                    </div>
-                </form>
-                <div className="section2">
-                    <img src={require("../../assets/img/login/사이드이미지.png")} />
                 </div>
+                {/*<div className="section2">*/}
+                {/*    <img src={require("../../assets/img/login/사이드이미지.png")} />*/}
+                {/*</div>*/}
             </div>
-        </div>
+        // </div>
     );
 }
 
